601036cc7f642c909679cd24f403704e61af19dd
Roberto de Souza <rabbittrix@hotmail.com>
2020-08-12 00:31:04 +0100

    Commit start of files of project.

---
+++ src/main/java/com/jrsf/SmartPoint/SmartPointApplication.java
@@ -0,0 +1,13 @@
+package com.jrsf.SmartPoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jrsf/SmartPoint/SmartPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrsf.SmartPoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
